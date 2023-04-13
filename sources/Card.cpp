2c293029be6eb81cbf9632ab2a654177d71656b0
Avichay3 <aviaimzina312@gmail.com>
2023-04-13 16:58:56 +0300

    end the Card class and changes to Game.hpp

--- sources/Card.cpp
+++ sources/Card.cpp
@@ -1,7 +1,129 @@
+/*The next code defines a class named Card with a private data members val of type cardValue and shape of type Shape.
+ * cardValue and Shape are both a two separate enums that define the possible values for a card (Ace, Two, Three, Four..) and the shape of the card (Leafs, Diamonds, Hearts, Clovers), respectively.
+
+The class has a default constructor that takes no arguments and sets the default value to Ace and Leafs.
+The class also has two public member functions, getCardValue() and getShape(), that return the value of the card and its shape, respectively.
+these functions are marked as const because they do not modify the state of the object.
+*/
+
 
 #include "card.hpp"
-using namespace ariel;
 
-Card::Card(int num_of_cards){ //constructor
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+namespace ariel {
+    Card::Card(cardValue val1, Shape shape1) : val(val1), shape(shape1) {}
+
+    cardValue Card::getCardValue() const {
+        return val;
+    }
+
+    Shape Card::getShape() const {
+        return shape;
+    }
+
+    string Card::cardValueToString(cardValue val) const {
+        string str_val;
+        switch (val) {
+            case Ace:
+                str_val = "Ace";
+                break;
+            case Two:
+                str_val = "Two";
+                break;
+            case Three:
+                str_val = "Three";
+                break;
+            case Four:
+                str_val = "Four";
+                break;
+            case Five:
+                str_val = "Five";
+                break;
+            case Six:
+                str_val = "Six";
+                break;
+            case Seven:
+                str_val = "Seven";
+                break;
+            case Eight:
+                str_val = "Eight";
+                break;
+            case Nine:
+                str_val = "Nine";
+                break;
+            case Ten:
+                str_val = "Ten";
+                break;
+            case Jack:
+                str_val = "Jack";
+                break;
+            case Queen:
+                str_val = "Queen";
+                break;
+            case King:
+                str_val = "King";
+                break;
+        }
+        return str_val;
+    }
+
+
+
+    /* the next function takes Shape value and convert it to string */
+    string Card::shapeToString(Shape shape) const {
+        string str_shape;
+        switch (shape) {
+            case Leafs:
+                str_shape = "Leafs";
+                break;
+            case Diamonds:
+                str_shape = "Diamonds";
+                break;
+            case Hearts:
+                str_shape = "Hearts";
+                break;
+            case Clovers:
+                str_shape = "Clovers";
+                break;
+        }
+        return str_shape;
+    }
+
+    /* A string representation for the card */
+    string Card::toString() const {
+        return cardValueToString(val) + " of " + shapeToString(shape);
+    }
+
+
+    /* the next six functions are gonna compare two cards objects based on their values */
+
+
+    bool Card::operator<(const Card &other) const {
+        return (this->val < other.val);
+    }
+
+    bool Card::operator>(const Card &other) const {
+        return (this->val > other.val);
+    }
+
+    bool Card::operator==(const Card &other) const {
+        return (this->val == other.val);
+    }
+
+    bool Card::operator!=(const Card &other) const {
+        return (this->val != other.val);
+    }
+
+    bool Card::operator<=(const Card &other) const {
+        return (this->val <= other.val);
+    }
+
+    bool Card::operator>=(const Card &other) const {
+        return (this->val >= other.val);
+    }
 
 }
--- sources/Game.hpp
+++ sources/Game.hpp
@@ -14,10 +14,26 @@ namespace ariel
         Player *winner; // reference to the winner of the game
         string lastTurnStats; // the last turn stats, we gonna use it in printLastTurn function
 
-        unsigned int turn; // current turn for player, it is unsigned beacause a turn can be between 0-26
+        unsigned int turn; // current turn for player, it is unsigned because a turn can be between 0-26
         unsigned int draws; // when both players play a card with the same value there is a draw.
         // this private member will keep truck of the number of draws in the game
         unsigned int p1Wins; // keep truck for the number of wins for player 1
         unsigned int p2Wins; // keep truck for the number of wins for player 2
+
+    public:
+        Game(Player& plr1, Player& plr2); //constructor with arguments the two players
+
+        void playTurn();
+
+        void printLastTurn() const { //prints the last turn stats
+            cout << lastTurnStats << endl;
+        }
+
+        void printWiner() const; // function to print the name of the winner
+
+
+        void printStats(); // function that prints the statistics of the game
+    };
+}
 
     }
