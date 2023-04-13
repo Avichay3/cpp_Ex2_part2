515468f5c6fd325546986df48d92118c74049da9
Avichay3 <aviaimzina312@gmail.com>
2023-04-13 21:44:45 +0300

    finish game.cpp

--- sources/Game.cpp
+++ sources/Game.cpp
@@ -9,29 +9,28 @@
 
 
 namespace ariel{
-    Game::Game(Player& player1, Player& player2) : p1(player1), p2(player2) , winner(nullptr) , lastTurnStats(""), turn(0), draws(0), player1Wins(0), player2Wins(0){
-
-        if(player1.isInTheGame()){
+    Game::Game(Player& player1, Player& player2) : p1(player1), p2(player2) , winner(nullptr) , lastTurnStats(""), turn(0), draws(0), player1Wins(0), player2Wins(0)) {
+        if (player1.isInTheGame()) {
             throw "player 1 is in the game";
         }
-        if(player2.isInTheGame()){
+        if (player2.isInTheGame()) {
             throw "player 2 is in the game";
         }
 
         Vector <Card> deck_of_cards;
-        for(int i = 1; i < 14; i++){
+        for (int i = 1; i < 14; i++) {
             deck_of_cards.push_back(Card(cardValue)i, Leafs);
         }
 
-        for(int i = 1; i < 14; i++){
+        for (int i = 1; i < 14; i++) {
             deck_of_cards.push_back(Card(cardValue)i, Diamonds);
         }
 
-        for(int i = 1; i < 14; i++){
+        for (int i = 1; i < 14; i++) {
             deck_of_cards.push_back(Card(cardValue)i, Hearts);
         }
 
-        for(int i = 1; i < 14; i++){
+        for (int i = 1; i < 14; i++) {
             deck_of_cards.push_back(Card(cardValue)i, Clovers);
         }
 
@@ -43,12 +42,11 @@ namespace ariel{
 
 
         int player = 1;
-        while(!cards.empty()){
-            if (player == 1){
+        while (!cards.empty()) {
+            if (player == 1) {
                 p1.addCard(cards.back());
                 player = 2;
-            }
-            else{
+            } else {
                 p2.addCard(cards.back());
                 player = 1;
             }
@@ -57,124 +55,125 @@ namespace ariel{
         this->p1.setInGame(true);
         this->p2.setInGame(true);
 
+
+        void Game::playAll() {
+            while (p1.isInGame() && p2.isInGame() && turn < 26) {
+                try {
+                    playTurn();
+                }
+                catch (const exception &e) {
+                    cerr << e.what() << endl;
+                    break;
+                }
+            }
+        }
+
         void Game::playTurn() {
-            if (!p1.isInGame() || !p2.isInGame()) { //// Throw an exception if the game is over
-                throw error("Game is over!");
-            }
+            if (&p1 == &p2)
+                throw error("Cannot play a game with the same player twice!");
+            if (!p1.isInGame() || !p2.isInGame())
+                throw logic_error("Game is over!");
+            if (++turn > 26)
+                throw logic_error("The game can't continue with more than 26 turns.");
 
-            turn++; // Increment turn counter
+            int draw_in_this_turn = 0, OnTable = 2;
+            lastTurnStats = "turn " + to_string(turn) + ":\n";
 
-            if (turn > 26) { // Throw an exception if the game has gone on for too many turns
-                throw error("Game cannot continue with more than 26 turns!");
-            }
-
-            /* Get cards from each player */
-            Card p1Card = p1.getCard();
-            Card p2Card = p2.getCard();
-
-            /* Remove cards from each player's hand */
-            p1.removeCard();
-            p2.removeCard();
-
-            // Add cards to the table
-            vector<Card> tableCards = {p1Card, p2Card};
-
-            // Handle a tie
-            while (p1Card == p2Card) {
-                // check if one of the players has no cards left in their stack or not enough cards to play
-                if (p1.stacksize() <= 1 || p2.stacksize() <= 1) {
-                    // divide the cards on the table evenly between the players
-                    int cardsOnTable = tableCards.size();
-                    int cardsPerPlayer = cardsOnTable / 2;
-                    for (int i = 0; i < cardsPerPlayer; i++) {
-                        p1.addCardTaken(tableCards[i]);
-                        p2.addCardTaken(tableCards[i + cardsPerPlayer]);
+            while (true) {
+                Card p1Card = p1.playCard();
+                Card p2Card = p2.playCard();
+                lastTurnStats += p1.getName() + " played " + p1Card.toString() + ", " + p2.getName() + " played " +
+                                 p2Card.toString() + ". ";
+                if (p1Card == p2Card) {
+                    draw_in_this_turn++;
+                    lastTurnStats += "draw!\n";
+                    if (p1.stacksize() <= 1 || p2.stacksize() <= 1) {
+                        while (OnTable > 0) {
+                            p1.addCardTaken();
+                            p2.addCardTaken();
+                            OnTable -= 2;
+                        }
+                        break;
                     }
+                    p1Card = p1.playCard();
+                    p2Card = p2.playCard();
+                    OnTable += 4;
+                    lastTurnStats += p1.getName() + " played " + p1Card.toString() + ", " + p2.getName() + " played " +
+                                     p2Card.toString() + ". ";
+                }
+
+                if (p1Card > p2Card) {
+                    lastTurnStats += p1.getName() + " has won the round.\n";
+                    p1.addCardTaken();
+                    p1.addCardTaken();
+                    OnTable -= 2;
+                } else if (p1Card < p2Card) {
+                    lastTurnStats += p2.getName() + " has won the round.\n";
+                    p2.addCardTaken();
+                    p2.addCardTaken();
+                    OnTable -= 2;
+                } else {
+                    lastTurnStats += "draw\n";
+                }
+
+                if (!p1.stacksize() || !p2.stacksize()) {
+                    p1.setInGame(false);
+                    p2.setInGame(false);
+                    winner = (p1.cardesTaken() > p2.cardesTaken()) ? &p1 : &p2;
                     break;
                 }
 
-                // Add cards to the table
-                tableCards.push_back(p1.getCard());
-                tableCards.push_back(p2.getCard());
-
-                // Remove cards from each player's hand
-                p1.removeCard();
-                p2.removeCard();
-
-                // Update the current cards being played
-                p1Card = tableCards.back();
-                p2Card = tableCards[tableCards.size() - 2];
-            }
-
-            // Determine the winner of the turn
-            if (p1Card > p2Card) {
-                // Player 1 wins the turn
-                p1Wins++;
-                for (Card c : tableCards) {
-                    p1.addCardTaken(c);
+                if (OnTable > 26) {
+                    throw error("Game cannot continue with more than 26 cards on the table!");
                 }
-                log += "Turn " + to_string(turn) + ":\n" + p1.getName() + " played " + p1Card.toString() + " " + p2.getName() + " played " + p2Card.toString() + ". " + p1.getName() + " won the round!\n";
-            }
-            else if (p2Card > p1Card) {
-                // Player 2 wins the turn
-                p2Wins++;
-                for (Card c : tableCards) {
-                    p2.addCardTaken(c);
-                }
-                log += "Turn " + to_string(turn) + ":\n" + p1.getName() + " played " + p1Card.toString() + " " + p2.getName() + " played " + p2Card.toString() + ". " + p2.getName() + " won the round!\n";
-            }
-            else {
-                // Tie
-                for (Card c : tableCards) {
-                    p1.addCardTaken(c);
-                }
-                log += "Turn " + to_string(turn) + ":\n" + p1.getName() + " played " + p1Card.toString() + " " + p2.getName() + " played " + p2Card.toString() + ". Draw!\n";
             }
 
-            // Determine if the game
+            lastTurnStats += "Draws in this turn: " + to_string(draw_in_this_turn) + "\n\n";
+            log += lastTurnStats;
+        }
 
 
+        void Game::playAll() {
+            while (p1.isInTheGame() && p2.isInTheGame() && this->turn < 26)
+                playTurn();
+        }
+
+        void Game::printWinner() const {
+            if (p1.isInGame() || p2.isInGame())
+                return;
+            int p1Score = p1.cardesTaken();
+            int p2Score = p2.cardesTaken();
+            cout << ((p1Score > p2Score) ? "The winner is " + p1.getName() + :
+                     (p1Score < p2Score) ? "The winner is " + p2.getName() + :
+                     "draw!") << endl;
+        }
 
 
+        void Game::printStats() {
+            // Display player 1's stats
+            cout << "Player " << p1.getName() << ":\n";
+            cout << "Cards won: " << p1.cardesTaken() << endl;
+            cout << "Cards left: " << p1.stacksize() << endl;
+            cout << "Win rate: " << fixed << setprecision(2) << (p1Wins / (float) turn) * 100 << "%" << endl;
 
-            this->player1 = player1;
-        this->player2 = player2;
-        player1.setIsPlaying(true);
-        player2.setIsPlaying(true);
-    }
+            // Display player 2's stats
+            cout << "Player " << p2.getName() << ":\n";
+            cout << "Cards won: " << p2.cardesTaken() << endl;
+            cout << "Cards left: " << p2.stacksize() << endl;
+            cout << "Win rate: " << fixed << setprecision(2) << (p2Wins / (float) turn) * 100 << "%" << endl;
 
+            // Display game statistics
+            cout << "Total turns: " << turn << endl;
+            cout << "Total draws: " << draws << endl;
+            cout << "Draw rate: " << fixed << setprecision(2) << (draws / (float) turn) * 100 << "%" << endl;
 
-    Game::Game(){ //another construnctor we gonna use
-    }
-
-
-    void Game::playTurn(){
-
-
-    }
-
-
-    void Game::printLastTurn(){
-
-
-    }
-
-
-
-    void Game::printWiner(){
-
-
-    }
+            // Display winning player (if applicable)
+            if (winner != nullptr) {
+                cout << "Winning player: " << winner->getName() << endl;
+            } else {
+                cout << "Winning player: Game is not finished!" << endl;
+            }
+        }
 
 
-    void Game::printLog(){
-
-
-    }
-
-
-    void Game::printStats(){
-
-    }
-}
 
